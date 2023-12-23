68ec555d80fcc1d3e8fc12249587c4758228db74
dream1ike <120911873+dream1ike@users.noreply.github.com>
2023-12-23 23:07:13 +0500

    Wrote description of functions

---
+++ TicTacToeBoard.py
@@ -0,0 +1,42 @@
+class TicTacToeBoard:
+    def __init__(self, size_board: int, player1: int, player2: int):
+        """
+        :param size_board: board = size_board * size_board
+        :param player1:  what will the first player play with
+        :param player2: what will the second player play with
+        """
+        self.size_board = size_board
+
+        # Fill board None
+        self.board = [[None for _ in range(size_board)] for _ in range(size_board)]
+
+        self.player1 = player1
+        self.player2 = player2
+        self.number_of_moves = 0
+
+    # Ф-я которая делает ход на доску (подаем в нее (x,y,player) и она заполняет доску
+    def make_move(self, x, y, player) -> int:
+        """
+        Fill the board
+        :param x: x-axis
+        :param y: y-axis
+        :param player: player1 or player2
+        :return: 0 - can not fill, 1 - filled success
+        """
+        result = 0
+        ...
+        self.number_of_moves += 1
+        return result
+
+    # Ф-я которая проверяет, выиграл ли кто-то (после size_board * 2 - 1 хода)
+    def check_game_status(self):
+        """
+        Check if someone's wins
+        :return: -1 - continue, 0 - draw, 1 - player1 wins, 2 - player2 wins
+        """
+
+    # Ф-я, которая возвращает доску
+    def get_board(self) -> list:
+        """
+        :return: board right now
+        """
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class TicTacToeBoard:
+    def __init__(self, a, b):
+        self.a = a
+        self.sum = None
+        self.b = b
+
+    def Printab(self):
+        print(f"a = {self.a}, b = {self.b}")
+
+    def CalcSum(self):
+        self.sum = self.a + self.b
+
+    def PrintSum(self):
+        print(f"sum = {self.sum}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+board = TicTacToeBoard(1,2)
 
+TicTacToeBoard.CalcSum(board)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+TicTacToeBoard.PrintSum(board)
